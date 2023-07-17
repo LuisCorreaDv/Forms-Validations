86b9e41d3b3fb07fed53278088c2b1019d544de0
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-07-17 16:31:12 -0600

    validity method and error messages

--- js/validaciones.js
+++ js/validaciones.js
@@ -6,6 +6,31 @@ export function valida(input) {
     if (validadores[tipoInput]) {
         validadores[tipoInput](input);
     }
+    //Input validity, se usa parentElement para dar target al div padre en el que se encuentra el input
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+    }else{
+        input.parentElement.classList.add("input-container--invalid")
+    }
+}
+
+const mensajesError = {
+    nombre: {
+        valueMissing: 'Este campo no puede estar vacío'
+    },
+    email: {
+        valueMissing: 'Este campo no puede estar vacío',
+        typeMismatch: 'El correo no es válido',
+    },
+    password: {
+        valueMissing: 'Este campo no puede estar vacío',
+        patternMismatch: 'Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.' 
+    },
+    nacimiento: {
+        valueMissing: 'Este campo no puede estar vacío',
+        customError: 'Debes tener al menos 18 años de edad'
+    }
 }
 
 //Asignando un objeto a los data tipes para seleccionar el tipo de input que recibe
